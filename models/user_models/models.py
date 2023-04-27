b15ce2ca47573149b4c5f280f8e466bb3db401e0
nurbol0tt <fairtail764@gmail.com>
2023-04-27 20:15:45 +0600

    improved the readability of the table

--- models/user_models/models.py
+++ models/user_models/models.py
@@ -78,23 +78,41 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey(
-        'user.id', ondelete="CASCADE"), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey(
-        'post.id', ondelete="CASCADE"), nullable=False)
+    author = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete="CASCADE"),
+        nullable=False
+    )
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey('post.id', ondelete="CASCADE"),
+        nullable=False
+    )
 
 
 class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.Integer, db.ForeignKey(
-        'user.id', ondelete="CASCADE"), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey(
-        'post.id', ondelete="CASCADE"), nullable=False)
+    author = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete="CASCADE"),
+        nullable=False
+    )
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey('post.id', ondelete="CASCADE"),
+        nullable=False
+    )
 
 
 class Saved(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.Integer, db.ForeignKey(
-        'user.id', ondelete="CASCADE"), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey(
-        'post.id', ondelete="CASCADE"), nullable=False)
+    author = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete="CASCADE"),
+        nullable=False
+    )
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey('post.id', ondelete="CASCADE"),
+        nullable=False
+    )
